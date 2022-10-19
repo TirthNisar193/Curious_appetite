9ed94a9c1ffe36a3afc6056a2f6ce1a7056726d3
TirthNisar193 <tirthnisar193@gmail.com>
2022-10-19 23:38:09 +0530

    first look

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 53, 165, 102);
+const kPrimaryLightColor = Color.fromRGBO(255, 243, 230, 1);
+
+const double defaultPadding = 16.0;
